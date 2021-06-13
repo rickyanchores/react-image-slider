00ba0930951a8ef3d36d3418e945b5803c893c7b
RickyAnchores <ricky23anchores@gmail.com>
2021-06-13 20:00:38 +0100

    finish

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,14 +6,33 @@ const ImageSlider = ({ slides }) => {
     const [current, setCurrent] = useState(0)
     const length = slides.length;
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1) ;
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1) ;
+    }
+
+
+    if(!Array.isArray(slides) || slides.length <= 0){
+        return null;
+    }
+
     return (
         <section className="slider">
-            <FaArrowAltCircleLeft className="left-arrow" />
-            <FaArrowAltCircleRight className="right-arrow" />
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
 
             {SliderData.map((slide, index) => {
-                return (
-                    <img src={slide.image} alt="" className="image"/>
+                return(
+                    <div 
+                    className={index === current ? 'slide active' : 'slide'} 
+                    key={index}>
+                        {index === current && (
+                            <img src={slide.image} alt="" className="image"/>
+                            )}
+                    </div>
                 );
             })}
         </section>
