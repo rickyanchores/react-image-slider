af11ef2c04545195fc888ce830201a7d6e0cd850
RickyAnchores <ricky23anchores@gmail.com>
2021-06-13 13:05:40 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import ImageSlider from "./components/ImageSlider";
+import { SliderData } from "./components/SliderData";
+import "./App.scss";
 
 
 function App() {
   return (
     <div className="App">
       <h1>React Image Slider</h1>
+      <ImageSlider slides={SliderData}/>
     </div>
   );
 }
---
+++ src/components/SliderData.js
@@ -0,0 +1,20 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmVvbnxlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1527698952067-61fc35796fcd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5lb258ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1594047752131-1ec0a6dfa4fc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fG5lb258ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1558795353-c39bb028827c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG5lb258ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1578290660132-0d8a28d46f13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fG5lb258ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1560049417-4490ff3a1f3d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fG5lb258ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60'
+    }
+]
